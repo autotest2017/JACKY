629641a260d7a53b69ede8ae7f4b8617054abb88
zhongjuwei <278122774@qq.com>
2017-02-26 09:58:06 +0800

    上传悬停与弹框的代码

---
+++ xuantingAndTankuang.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# coder: JACKY
+# version: 1.0
+
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+wd = webdriver.Firefox()
+wd.get("http://www.baidu.com")
+link = wd.find_element_by_link_text(u"设置")
+tmp = ActionChains(wd).move_to_element(link)
+tmp.perform()
+
+ActionChains(wd).double_click(link).perform()
+ActionChains(wd).click(link).perform()
+wd.find_element_by_link_text(u"搜索设置").click()
+wd.find_element_by_class_name("prefpanelgo").click()
+
+#wd.switch_to_alert().accept()
+wd.switch_to.alert.accept()
